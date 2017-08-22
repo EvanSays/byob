2c650da7e206a13c22f4065e71148403d8e5870e
Evan Miller <evanmillerny@gmail.com>
2017-08-22 16:43:47 -0600

    add development migration

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/goodbooks',
+    connection: 'postgres://localhost/crispr',
     migrations: {
       directory: './db/migrations'
     },
