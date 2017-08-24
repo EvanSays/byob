52999fd08f0af68a58a773d2041fc08c90c9c2c8
Evan Miller <evanmillerny@gmail.com>
2017-08-24 15:47:24 -0600

    add env file for secret key

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,4 @@
 const baseRoute = 'http://localhost:6333/';
-
 const appName   = '/byob/';
 const api       = 'api/';
 const version   = 'v1/';
@@ -7,10 +6,7 @@ const version   = 'v1/';
 const journals  = `${appName}${api}${version}journals/`;
 const genes     = `${appName}${api}${version}genes/`;
 
-const secretKey = 'superSecretMashHas'
-
 module.exports = {
-  secretKey,
   getAllJournals: journals,
   getAllGenes: genes,
   getGenes_pubmed: `${genes}$/:pubmed`
