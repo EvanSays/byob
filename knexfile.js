4e9038ebde6694255d4381b76c0906e4cfd660b2
Daniel Bucket <danieljbucket@gmail.com>
2017-08-23 18:19:08 -0600

    circle.yml file creation and knexfile.js update. app should be ready for circle.ci intedgration

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,7 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
@@ -18,4 +19,16 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/jetfueltest',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/test/seeds'
+    }
+  }
 };
