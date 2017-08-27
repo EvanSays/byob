3c05a8db18de7818be78bfc0b2c329941b5b67a6
Daniel Bucket <danieljbucket@gmail.com>
2017-08-26 18:22:54 -0600

    removed a blank line. 60 commits to go

--- test/genesServerTest.spec.js
+++ test/genesServerTest.spec.js
@@ -122,7 +122,6 @@ describe('API Routes', () => {
     })
   })
 
-
   describe('GET /journals/:pubmed/genes', () => {
     it('01: should get a gene response based on the pubmed id', (done) => {
       chai.request(server)
